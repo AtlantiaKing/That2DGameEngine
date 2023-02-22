fc2df81425e9ba474738f7deb6347c8c77efe360
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-02-22 17:35:46 +0100

    Made GameObject final
    Made GameObject class final
    Commented everything GameObject related because it breaks by removing inheritance on the GameObject

--- That2DGameEngine/GameObject.cpp
+++ That2DGameEngine/GameObject.cpp
@@ -7,18 +7,4 @@ that::GameObject::~GameObject() = default;
 
 void that::GameObject::Update(){}
 
-void that::GameObject::Render() const
-{
-	const auto& pos = m_transform.GetPosition();
-	Renderer::GetInstance().RenderTexture(*m_texture, pos.x, pos.y);
-}
-
-void that::GameObject::SetTexture(const std::string& filename)
-{
-	m_texture = ResourceManager::GetInstance().LoadTexture(filename);
-}
-
-void that::GameObject::SetPosition(float x, float y)
-{
-	m_transform.SetPosition(x, y, 0.0f);
-}
+void that::GameObject::Render() const {}
--- That2DGameEngine/GameObject.h
+++ That2DGameEngine/GameObject.h
@@ -4,28 +4,19 @@
 
 namespace that
 {
-	class Texture2D;
-
-	// todo: this should become final.
-	class GameObject 
+	class GameObject final
 	{
 	public:
 		virtual void Update();
 		virtual void Render() const;
 
-		void SetTexture(const std::string& filename);
-		void SetPosition(float x, float y);
-
 		GameObject() = default;
 		virtual ~GameObject();
+
 		GameObject(const GameObject& other) = delete;
 		GameObject(GameObject&& other) = delete;
 		GameObject& operator=(const GameObject& other) = delete;
 		GameObject& operator=(GameObject&& other) = delete;
-
 	private:
-		Transform m_transform{};
-		// todo: mmm, every gameobject has a texture? Is that correct?
-		std::shared_ptr<Texture2D> m_texture{};
 	};
 }
