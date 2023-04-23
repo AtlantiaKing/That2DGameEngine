39826256f47646a1695e00f83cb636557ece020b
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-04-23 11:31:53 +0200

    Added CyclicBuffer class
    Added templated CyclicBuffer class

---
+++ That2DGameEngine/CyclicBuffer.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Logger.h"
+
+#include <sstream>
+
+namespace that
+{
+	template <typename T, int size>
+	class CyclicBuffer
+	{
+	public:
+		void Insert(T&& item);
+		void Insert(const T& item);
+		T Pop();
+		unsigned int GetNrAssigned() { return m_NrItems; }
+	private:
+		T m_Buffer[size]{};
+
+		unsigned int m_CurItemIdx{};
+		unsigned int m_NrItems{};
+	};
+	template<typename T, int size>
+	inline void CyclicBuffer<T, size>::Insert(T&& item)
+	{
+		if (m_NrItems >= size)
+		{
+			std::stringstream sstream{};
+			sstream << "Too many items are being queued. Item \"" <<  item << "\" was not added to the event queue.";
+			Logger::LogWarning(sstream.str());
+			return;
+		}
+
+		const unsigned int curIdx{ (m_CurItemIdx + m_NrItems) % size };
+		m_Buffer[curIdx] = std::move(item);
+		++m_NrItems;
+	}
+
+	template<typename T, int size>
+	inline void CyclicBuffer<T, size>::Insert(const T& item)
+	{
+		if (m_NrItems >= size)
+		{
+			std::stringstream sstream{};
+			sstream << "Too many items are being queued. Item \"" << item << "\" was not added to the event queue.";
+			Logger::LogWarning(sstream.str());
+			return;
+		}
+
+		const unsigned int curIdx{ (m_CurItemIdx + m_NrItems) % size };
+		m_Buffer[curIdx] = item;
+		++m_NrItems;
+	}
+
+	template<typename T, int size>
+	inline T CyclicBuffer<T, size>::Pop()
+	{
+		unsigned int curIdx{ m_CurItemIdx };
+
+		++m_CurItemIdx %= size;
+		--m_NrItems;
+
+		return std::move(m_Buffer[curIdx]);
+	}
+}
